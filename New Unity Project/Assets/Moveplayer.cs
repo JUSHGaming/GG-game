e1008f58f365fba9ffaff7b1bc08c8ed6707c48e
JUSHGaming <jushgaming@gmail.com>
2015-12-09 15:37:48 +0100

    9/12 15
    added cancer to game

---
+++ New Unity Project/Assets/Moveplayer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Moveplayer : MonoBehaviour {
+
+	public Rigidbody player;
+	public float speed = 10f;
+	public float jumpHeight = 5f;
+	private Vector3 velocity;
+	private bool canJump;
+	// Update is called once per frame
+	void Update () {
+		velocity = Vector3.zero;
+
+		velocity += new Vector3 (Input.GetAxis ("Horizontal"), 0, 0) * speed * Time.deltaTime;
+		player.transform.position += velocity;
+	
+		if ((Input.GetKeyDown (KeyCode.W) || Input.GetKeyDown (KeyCode.Space)) && canJump) 
+		{
+			player.velocity += Vector3.up * jumpHeight; 	
+
+		}
+	}
+
+	public void setCanJump(bool canJump){
+		this.canJump = canJump;
+	}
+}
---
+++ New Unity Project/Assets/groundcheckscript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class groundcheckscript : MonoBehaviour {
+
+	public Moveplayer player;
+	
+	// Update is called once per frame
+	void OnTriggerEnter (Collider triggerd){
+		player.setCanJump(true);
+	}
+
+	void OnTriggerExit (Collider trigger){
+		player.setCanJump(false);
+	}
+}
